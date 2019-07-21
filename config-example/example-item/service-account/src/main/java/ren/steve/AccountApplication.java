3143ae49a44c721d1500e0bcc2ecb065085228d6
liuguojia <liuguojia@meishubao.qq.com>
2019-07-21 14:41:28 +0800

    configuration cloud service account

---
+++ config-example/example-item/service-account/src/main/java/ren/steve/AccountApplication.java
@@ -0,0 +1,21 @@
+package ren.steve;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author : 刘国家
+ * @Date : 2019-07-21 14:33
+ * @Description :
+ * @ModifiedBy :
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("ren.steve.account.mapper")
+public class AccountApplication {
+  public static void main ( String[] args ) {
+    SpringApplication.run(AccountApplication.class);
+  }
+}
