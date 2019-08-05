1b7f0fd481db43bb7ff9eb7bb60b911c8f82d6b1
liuguojia <liuguojia>
2019-08-05 15:42:00 +0800

    不提交iml文件

---
+++ config-example/example-common/src/main/java/ren/steve/common/utils/DateFormatUtil.java
@@ -0,0 +1,26 @@
+package ren.steve.common.utils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * @Author : 刘国家
+ * @Date : 2019-07-22 12:54
+ * @Description : 处理数据的日期格式，将所有处理对象内的日期格式转换成时间戳
+ * @ModifiedBy :
+ */
+
+
+public class DateFormatUtil {
+
+
+  public Object convertDateToTimestamp( Object object ) {
+
+
+
+    return object;
+  }
+
+
+}
